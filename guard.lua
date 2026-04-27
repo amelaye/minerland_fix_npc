ce66aab9e9af83dc27dff49f457311f34ee17615
Amelie DUVERNET <amelieonline@gmail.com>
2026-04-27 20:10:11 +0200

    garde menacé - correction visuels

--- guard.lua
+++ guard.lua
@@ -86,7 +86,7 @@ local function attach_taurus(guard_obj)
 	if not ent then return nil end
 	ent:set_attach(guard_obj, "Arm_Right",
 		{x = 0, y = 6, z = 0},   -- position dans le bone
-		{x = 180, y = 90, z = 0} -- orientation flip vertical
+		{x = 225, y = 0, z = 90}  -- orientation
 	)
 	return ent
 end
