d8044fe96618258ad85ade1c75c115d8444f0d13
Amelie DUVERNET <amelieonline@gmail.com>
2026-04-28 21:50:32 +0200

    Riposte du joueur fly

--- guard.lua
+++ guard.lua
@@ -138,19 +138,15 @@ core.register_entity("minerland_fix_npc:guard_bullet", {
 				self._hit = true
 				-- retire le fly 30s si le joueur vole
 				local pname = obj:get_player_name()
-				local privs = core.get_player_privs(pname)
-				local had_fly = privs.fly
-				if had_fly then
-					privs.fly = false
-					core.set_player_privs(pname, privs)
-					core.after(30, function()
-						local p = core.get_player_by_name(pname)
-						if p then
-							privs.fly = true
-							core.set_player_privs(pname, privs)
-						end
-					end)
-				end
+				local phys = obj:get_physics_override()
+				local was_flying = phys and phys.fly
+				obj:set_physics_override({fly = false})
+				core.after(30, function()
+					local p = core.get_player_by_name(pname)
+					if p then
+						p:set_physics_override({fly = true})
+					end
+				end)
 				-- gros dégâts directs
 				local hp = obj:get_hp()
 				obj:set_hp(math.max(0, hp - self._damage))
